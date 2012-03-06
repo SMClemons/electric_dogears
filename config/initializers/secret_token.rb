a8c4dd1b72b31ffd2ef735621569dfb2dff8ff3b
Steve Clemons <clemons@matcmadison.edu>
2012-03-05 18:20:18 -0600

    starting project one

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElectricDogears::Application.config.secret_token = 'decf8fd8f64ce99fe25af107b0986c85554b42b64ff1b86131ab71fd0c0e0586254b7b16f4169c8c89a7da98ce9c4cfcc34929c30b78df5f24176fafbbdcdee3'
